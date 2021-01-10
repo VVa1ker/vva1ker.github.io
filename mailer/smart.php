2f703753418e64904412c1f1bae8856f1c8042c0
VVa1ker <50447823+VVa1ker@users.noreply.github.com>
2021-01-10 15:35:24 +0100

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'a1en.vva1ker@gmail.com';                 // Наш логин
-$mail->Password = '1994123alenwalker';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
